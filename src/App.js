d2c4a2c7d5b418d7f125bba3811832f911c45734
cvsamsad <cvsamsad@gmail.com>
2020-12-24 00:17:01 +0400

    Add Todo prevTodo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState,useRef} from 'react';
 import  TodoList  from './TodoList';
 
 function App() {
   const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e){
+      const name = todoNameRef.current.value
+      if(name === ''){
+        return
+      }
+      setTodos(prevTodos =>{
+        return [...prevTodos,{id:1,name:name,complete:false}]
+      })
+      console.log(name)
+      todoNameRef.current.value = ''
+  }
+
   return (
     <>
     <TodoList todoList={todos}/>
-    <input type="text"/>
-    <button>Add Todo</button>
+    <input ref={todoNameRef} type="text"/>
+    <button onClick={handleAddTodo}>Add Todo</button>
     <button>Clear Complted Todo</button>
     <div>0 left to do</div>
     </>
